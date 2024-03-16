0f97d53991f66c3d3b1a9866f46486447a494fe4
Beau <beau28713@gmail.com>
2024-03-16 11:51:43 -0400

    testing platformio

---
+++ README.md
@@ -0,0 +1,44 @@
+## Why I'm doing the project  
+I started this project to help my father keep track of his camper’s water supply. My father had to check the water level every morning and make
+sure the flow from the spring hadn’t stopped. If the flow stopped, he would need to manually insert a water house into the pipe and 
+Back flush the water lines to rid any sand that may have stopped up the line. We installed screens at the spring house to help catch most of the sand  
+but some would still make it through. The lines would also become airlocked (I have a plane for this) and the backflushing would rid the lines of this blockage. 
+
+
+## About the project
+In the early stages I used an incandescent lamp in series with a tilt switch. This would give my father a visual indication as to when the water level  
+was below the threshold and needed attention. But this didn’t tell him what the problem was, it just told him there was a problem that needed 
+attention. I wanted a system that would automate the whole process for him with as little human interaction as possible. So, I started working  
+on a modular automated system that would do the things he needed. 
+
+I wanted to track water flow and water level also keeping things open and cable of upgrading/dating the system and capabilities in the future.  
+
+## How it works
+Utilizing an Arduino as the brain, the system monitors the water level and flow via a tilt switch, float, and a water flow sensor. When the water 
+level falls low enough the tilt switch makes contact and the water level input pin on the Arduino is pulled low. This pin is read by the Arduino, 
+relay 1 is then activated thus activating a servo that switches the position of a 3-way water valve blocking flow into the holding tank and opening 
+access to the water lines. Five seconds later the Arduino activates relay 2 turning on a pump. This pump feeds water back into the water line 
+backflushing the lines and removing all sand and airlocks. After running for one minute relay 2 deactivate turning off the pump. After 5 seconds 
+relay 1 deactivates, this activates the 3-way water valve moving it back into its home position allowing water from the spring to once again 
+flow into the holding tanks.  
+The system also has a manual mode where at any time the sequence of operation can be performed when desired. 
+
+## Current test bed set up for prototyping and testing.  
+![Water_Flow_added](https://github.com/Beau28713/Arduino_BackFlush_Control/assets/65408911/e702b7a6-e988-44c8-b125-59c5cffbebe8)
+
+
+
+## Field units 
+[3-way water valve](https://www.aliexpress.us/item/3256804648845790.html?spm=a2g0o.order_list.order_list_main.5.695718020k35QP&gatewayAdapt=glo2usa)    
+[5GPM submergible pump](https://www.amazon.com/dp/B09ZV2364K?psc=1&ref=ppx_yo2ov_dt_b_product_details)    
+[water flow meter](https://www.adafruit.com/product/5066)     
+[4 relay board](https://www.amazon.com/dp/B00E0NSORY?ref=ppx_yo2ov_dt_b_product_details&th=1)  
+[Water circulator pump](https://www.amazon.com/dp/B0196WL55G?psc=1&ref=ppx_yo2ov_dt_b_product_details) *For testing water flow sensor  
+
+## Control and display:
+-[Arduino Uno wifi](https://store-usa.arduino.cc/collections/boards-modules/products/arduino-uno-wifi-rev2?_pos=5&_fid=1e131e922&_ss=c)  
+-[AdaFruit LCD Screen with I2C backpack](https://www.adafruit.com/product/292)    
+-Various buttons and switches  
+
+## Power:
+120 input -> 12DC output power supply
---
+++ src/Arduino_BackFlush_Control.ino
@@ -0,0 +1,203 @@
+
+/*
+  Adafruit BackPack I2C connection
+  The circuit:
+ * 5V to Arduino 5V pin
+ * GND to Arduino GND pin
+ * CLK to Analog #5
+ * DAT to Analog #4
+*/
+
+#include "Adafruit_LiquidCrystal.h"
+#include <Arduino.h>
+
+// Connect via i2c, default address #0 (A0-A2 not jumpered)
+Adafruit_LiquidCrystal lcd(0);
+
+int valve_control_relay = 8;
+int pump_control_relay = 12;
+int flow_sensor = 5;
+
+int tilt_switch_position = 0;
+int man_backflush_switch_position = 0;
+int manual_auto_switch_position = 0;
+
+int man_backflush_switch = 2;
+int manual_auto_control_switch = 13;
+int tilt_switch = 4;
+
+unsigned long previous_time = 0;
+unsigned long prev_time = 0;
+unsigned long weekly_timer;
+unsigned long count_timer;
+
+unsigned long time_interval = 1000;
+unsigned long weekly_interval = 604800000; // one week in Milliseconds
+int back_flushes_completed = 0;
+
+volatile uint8_t lastflowpinstate;
+volatile uint16_t pulses = 0; // number of pulses
+uint8_t flow;
+volatile float flowrate;      // flow rate
+volatile uint32_t lastflowratetimer = 0;
+
+SIGNAL(TIMER0_COMPA_vect)
+{
+  flow = digitalRead(flow_sensor);
+  
+  if (flow == lastflowpinstate) {
+    lastflowratetimer++;
+    return; // nothing changed!
+  }
+
+
+  lastflowpinstate = flow;
+  flowrate = 1000.0;
+  flowrate /= lastflowratetimer; // in hertz
+  lastflowratetimer = 0;
+}
+
+void setup()
+{
+  pinMode(tilt_switch, INPUT_PULLUP);
+  pinMode(man_backflush_switch, INPUT_PULLUP);
+  pinMode(manual_auto_control_switch, INPUT_PULLUP);
+  pinMode(valve_control_relay, OUTPUT);
+  pinMode(pump_control_relay, OUTPUT);
+  pinMode(flow_sensor, INPUT);
+  digitalWrite(flow_sensor, HIGH);
+  useInterrupt(true);
+
+  Serial.begin(9600);
+
+  digitalWrite(valve_control_relay, HIGH);
+  digitalWrite(pump_control_relay, HIGH);
+
+  // set up the LCD's number of rows and columns:
+  if (!lcd.begin(20, 4))
+  {
+    Serial.println("Could not init backpack. Check wiring.");
+    while (1)
+      ;
+  }
+
+  lcd.clear();
+}
+
+void loop()
+{
+  weekly_timer = millis();
+
+  tilt_switch_position = digitalRead(tilt_switch);
+  manual_auto_switch_position = digitalRead(manual_auto_control_switch);
+
+  if (manual_auto_switch_position == LOW)
+  {
+    man_backflush_switch_position = digitalRead(man_backflush_switch);
+    lcd.setCursor(0, 0);
+    lcd.print("Manual Mode");
+
+    if (man_backflush_switch_position == LOW)
+    {
+      valve_open();
+      pump_on();
+      pump_off();
+      valve_close();
+      back_flushes_completed += 1;
+    }
+  }
+
+  else if (manual_auto_switch_position == HIGH)
+  {
+    lcd.setCursor(0, 0);
+    lcd.print("Auto Mode  ");
+    if (weekly_timer - prev_time >= weekly_interval)
+    {
+      prev_time = weekly_timer;
+      valve_open();
+      pump_on();
+      pump_off();
+      valve_close();
+      back_flushes_completed += 1;
+    }
+  }
+
+  if (flowrate < 12)
+  {
+    lcd.setCursor(0, 3);
+    lcd.print("No Flow: ");
+  }
+  else
+  {
+    lcd.setCursor(0, 3);
+    lcd.print("Flow:    ");
+  }
+  lcd.print(flowrate);
+  
+
+  lcd.setCursor(0, 2);
+  lcd.print("BF completed: ");
+  lcd.print(back_flushes_completed);
+}
+
+void useInterrupt(boolean v)
+{
+  if (v)
+  {
+    OCR0A = 0xAF;
+    TIMSK0 |= _BV(OCIE0A);
+  }
+  else
+  {
+    // do not call the interrupt function COMPA anymore
+    TIMSK0 &= ~_BV(OCIE0A);
+  }
+}
+
+void valve_open()
+{
+  lcd.setCursor(0, 1);
+  lcd.print("BF valve opening  ");
+  digitalWrite(valve_control_relay, LOW);
+  count_down_timer(15);
+}
+
+void valve_close()
+{
+  lcd.setCursor(0, 1);
+  lcd.print("BF valve closing  ");
+  digitalWrite(valve_control_relay, HIGH);
+  count_down_timer(15);
+  lcd.clear();
+}
+
+void pump_on()
+{
+  lcd.setCursor(0, 1);
+  lcd.print("Pump on         ");
+  digitalWrite(pump_control_relay, LOW);
+  count_down_timer(15);
+}
+
+void pump_off()
+{
+  lcd.setCursor(0, 1);
+  lcd.print("Pump off        ");
+  digitalWrite(pump_control_relay, HIGH);
+  count_down_timer(5);
+}
+
+void count_down_timer(int timer)
+{
+  int time_count = 0;
+  while (time_count < timer)
+  {
+    count_timer = millis();
+
+    if (count_timer - previous_time >= time_interval)
+    {
+      previous_time = count_timer;
+      time_count += 1;
+    }
+  }
+}
